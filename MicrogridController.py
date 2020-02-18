4bcd03750641874eec38c58640f9cd5a3e71a86f
phna001 <hnathwani@epri.com>
2020-02-17 21:01:27 -0800

    microgrid controller initializes reliability and inherits from storagevet controller

--- MicrogridController.py
+++ MicrogridController.py
@@ -7,6 +7,7 @@ __email__ = ['hnathwani@epri.com', 'egiarta@epri.com', 'mevans@epri.com']
 __version__ = "x.x.x"
 
 from storagevet.Controller import Controller
+from ValueStreamsDER.Reliability import Reliability
 
 
 class MicrogridController(Controller):
@@ -14,3 +15,19 @@ class MicrogridController(Controller):
     into energy markets
 
     """
+
+    def __init__(self, reliability, deferral, dr, ra, backup, volt, user, daets, fr, lf, sr, nsr, dcm, retailets):
+        """Initialized the value streams that the DERs will be evaluated under"""
+        super().__init__(deferral, dr, ra, backup, volt, user, daets, fr, lf, sr, nsr, dcm, retailets)
+        self.inputs_map['Reliability'] = reliability
+
+    def initialize_valuestreams(self, opt_years, frequency):
+        if self.inputs_map['Reliability']:
+            inputs = self.inputs_map['Reliability']
+            new_service = Reliability(inputs)
+            new_service.estimate_year_data(opt_years, frequency)
+            self.value_streams['Reliability'] = new_service
+            self.inputs_map.pop('Reliability')
+
+        super().initialize_valuestreams(opt_years, frequency)
+

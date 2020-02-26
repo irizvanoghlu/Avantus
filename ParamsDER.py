293be0fc573a5fdd839a250986177ff2e018e2d1
phna001 <hnathwani@epri.com>
2020-02-26 10:12:04 -0800

    WIP: moving evaluation column reading into Params from CBA

--- ParamsDER.py
+++ ParamsDER.py
@@ -32,6 +32,9 @@ class ParamsDER(Params):
     """
     # set schema loction based on the location of this file (this should override the global value within Params.py
     schema_location = os.path.abspath(__file__)[:-len('ParamsDER.py')] + "SchemaDER.xml"
+    cba_input_error_raised = False
+
+    cba_input_template = None
 
     @staticmethod
     def csv_to_xml(csv_filename, verbose=False, ignore_cba_valuation=False):
@@ -93,14 +96,27 @@ class ParamsDER(Params):
 
             Returns dictionary of instances of Params, each key is a number
         """
-        cls.instances = super().initialize(filename, verbose)
-        # 1) determine if cba value can be given and validate
+        cls.instances = super().initialize(filename, verbose)  # everything that initialize does in Params (steps 1-4)
 
-        # 2) convert any referenced data into direct data
+        # 5) load direct data and create input template
+        # determine if cba value can be given and validate
+        cls.cba_input_template = cls.cba_template_struct()
 
-        # 3) if sensitivity analysis, then make sure enough cba values are given
+        # convert any referenced data into direct data (add referenced data to dict DATASETS)
+        cls.read_evaluation_referenced_data()
 
-        # 4) build a dictionary of CBA inputs that match with the instances that need to be run
+        # report back any warning associated with the 'Evaulation' column
+        if cls.cba_input_error_raised:
+            raise AssertionError("The model parameter has some errors associated to it in the CBA column. Please fix and rerun.")
+
+        # 6) if SA, update case definitions to define which CBA values will apply for each case
+        cls.add_evaluation_to_case_definitions()
+
+        # 7) build a dictionary of CBA inputs that matches with the instance Params that the inputs should be paired with and
+        # load up datasets that correspond with referenced data in respective cba_input_instance
+
+        # distribute CBA dictionary of inputs to the corresponding Param instance (so its value can be passed on to Scenario)
+
 
         return cls.instances
 
@@ -109,6 +125,186 @@ class ParamsDER(Params):
         """
         super().__init__()
         self.Reliability = self.read_and_validate('Reliability')
+
+    @classmethod
+    def cba_template_struct(cls):
+        """
+
+        Returns: a template structure that summarizes the inputs for a CBA instance
+
+        """
+        template = dict()
+        template['Scenario'] = cls.read_and_validate_cba('Scenario')
+        template['Finance'] = cls.read_and_validate_cba('Finance')
+
+        # create dictionary for CBA values for DERs
+        template['ders_values'] = {
+            'Battery': cls.read_and_validate_cba('Battery'),
+            'PV': cls.read_and_validate_cba('PV'),  # cost_per_kW (and then recalculate capex)
+            'ICE': cls.read_and_validate_cba('ICE')  # fuel_price,
+        }
+
+        # create dictionary for CBA values for all services (from data files)
+        template['valuestream_values'] = {'User': cls.read_and_validate_cba('User')}  # USER will only have one entry in it (key = price)
+        return template
+
+    @classmethod
+    def read_and_validate_cba(cls, name):
+        """ Read data from valuation XML file
+
+        Args:
+            name (str): name of root element in xml file
+
+        Returns: A dictionary filled with values provided by user that will be used by the CBA class
+        or None if no values are active.
+
+        """
+        tag = cls.xmlTree.find(name)
+        schema_tag = cls.schema_tree.find(name)
+
+        # check to see if user includes the tag within the provided xml
+        if tag is None:
+            return
+
+        # This statement checks if the first character is 'y' or '1', if true it creates a dictionary.
+        if tag.get('active')[0].lower() == "y" or tag.get('active')[0] == "1":
+            # Check if tag is in schema
+            if schema_tag is None:
+                cls.report_warning("missing tag", tag=name, raise_input_error=False)
+                # warn user that the tag given is not in the schema
+                return
+            dictionary = {}
+            # iterate through each tag required by the schema
+            for schema_key in schema_tag:
+                # Check if attribute is in the schema
+                try:
+                    key = tag.find(schema_key.tag)
+                    cba_value = key.find('Evaluation')
+                except (KeyError, AttributeError):
+                    cls.report_warning("missing key", tag=name, key=schema_key.tag)
+                    continue
+                # if we dont have a cba_value, skip to next key
+                if cba_value is None:
+                    continue
+                # check if the first character is 'y' for the active value within each property
+                if cba_value.get('active')[0].lower() == "y" or cba_value.get('active')[0] == "1":
+                    valuation_entry = None
+                    intended_type = key.find('Type').text
+                    if key.get('analysis')[0].lower() == 'y' or key.get('analysis')[0].lower() == '1':
+                        # if analysis, then convert each value and save as list
+                        tag_key = (tag.tag, key.tag)
+                        sensitivity_values = cls.extract_data(key.find('Evaluation').text, intended_type)
+
+                        # validate each value
+                        for values in sensitivity_values:
+                            cls.checks_for_validate(values, schema_key, name)
+
+                        #  check to make sure the length match with sensitivity analysis value set length
+                        required_values = len(cls.sensitivity['attributes'][tag_key])
+                        if required_values != len(sensitivity_values):
+                            cls.report_warning('cba sa length', tag=name, key=key.tag, required_num=required_values)
+                        cls.sensitivity['cba_values'][tag_key] = sensitivity_values
+                    else:
+                        # convert to correct data type
+                        valuation_entry = cls.convert_data_type(key.find('Evaluation').text, intended_type)
+                    # save evaluation value OR save a place for the sensitivity value to fill in the dictionary later w/ None
+                    dictionary[key.tag] = valuation_entry
+                else:
+                    cls.report_warning('cba not allowed', tag=name, key=key.tag, raise_input_error=False)
+            return dictionary
+
+    @classmethod
+    def report_warning(cls, warning_type, raise_input_error=True, **kwargs):
+        """ Print a warning to the user log. Warnings are reported, but do not result in exiting.
+
+        Args:
+            warning_type (str): the classification of the warning to be reported to the user
+            raise_input_error (bool): raise this warning as an error instead back to the user and stop running
+                the program
+            kwargs: elements about the warning that need to be reported to the user (like the tag and key that
+                caused the error
+
+        """
+
+        if warning_type == 'cba not allowed':
+            e_logger.error(f"INPUT: {kwargs['tag']}-{kwargs['key']} is not be used within the " +
+                           "CBA module of the program. Value is ignored.")
+            cls.cba_input_error_raised = raise_input_error or cls.cba_input_error_raised
+        if warning_type == "cba sa length":
+            cls.cba_input_error_raised = raise_input_error or cls.cba_input_error_raised
+            e_logger.error(f"INPUT: {kwargs['tag']}-{kwargs['key']} has not enough CBA evaluatino values to "
+                           f"successfully complete sensitivity analysis. Please include {kwargs['required_num']} "
+                           f"values, each corresponding to the Sensitivity Analysis value given")
+        super().report_warning(warning_type, raise_input_error, **kwargs)
+
+    @classmethod
+    def read_evaluation_referenced_data(cls):
+        """ This function makes a unique set of filename(s) based on grab_evaluation_lst and the data already read into REFERENCED_DATA.
+            It applies for time series filename(s), monthly data filename(s), customer tariff filename(s).
+            For each set, the corresponding class dataset variable (ts, md, ct) is loaded with the data.
+
+            Preprocess monthly data files
+
+        """
+
+        ts_files = cls.grab_evaluation_lst('Scenario', 'time_series_filename') - {cls.referenced_data['time_series'].keys()}
+        md_files = cls.grab_evaluation_lst('Scenario', 'monthly_data_filename') - {cls.referenced_data['monthly_data'].keys()}
+        ct_files = cls.grab_evaluation_lst('Finance', 'customer_tariff_filename') - {cls.referenced_data['customer_tariff'].keys()}
+        yr_files = cls.grab_evaluation_lst('Finance', 'yearly_data_filename') - {cls.referenced_data['yearly_data'].keys()}
+
+        for ts_file in ts_files:
+            cls.referenced_data['time_series'][ts_file] = cls.read_from_file('time_series', ts_file, 'Datetime (he)')
+        for md_file in md_files:
+            cls.referenced_data['monthly_data'][md_file] = cls.preprocess_monthly(cls.read_from_file('monthly_data', md_file, ['Year', 'Month']))
+        for ct_file in ct_files:
+            cls.referenced_data['customer_tariff'][ct_file] = cls.read_from_file('customer_tariff', ct_file, 'Billing Period')
+        for yr_file in yr_files:
+            cls.referenced_data['yearly_data'][yr_file] = cls.read_from_file('yearly_data', yr_file, 'Year')
+
+        return True
+
+    @classmethod
+    def grab_evaluation_lst(cls, tag, key):
+        """ Checks if the tag-key exists in cls.sensitivity, otherwise grabs the base case value
+        from cls.template
+
+        Args:
+            tag (str):
+            key (str):
+
+        Returns: set of values
+
+        """
+        try:
+            values = set(cls.sensitivity['cba_values'][(tag, key)])
+        except KeyError:
+            try:
+                values = {cls.cba_input_template[tag][key]}
+            except TypeError:
+                values = {}
+        return values
+
+    @classmethod
+    def add_evaluation_to_case_definitions(cls):
+        """ Method that adds the 'Evaluation' values as a column to the dataframe that defines the differences in the cases
+        being run.
+
+        """
+        cba_sensi = cls.sensitivity['cba_values']
+        # for each tag-key cba value that sensitivity analysis applies to
+        for tag_key, value_lst in cba_sensi:
+            # initialize the new column with 'NaN'
+            cls.case_definitions[f"CBA {tag_key}"] = np.NAN
+            # get the number of values that you will need to iterate through
+            num_cba_values = len(value_lst)
+            # for each index in VALUE_LST
+            for index in range(num_cba_values):
+                # find the row(s) that contain the optimization value that was also the INDEX-th value in the Sensitivity Parameters entry
+                cls.case_definitions.loc[cls.case_definitions[tag_key] == cls.sensitivity['attributes'][tag_key][index], f"CBA {tag_key}"] = value_lst[index]
+
+        # check for any entries w/ NaN to make sure everything went fine
+        if np.any(cls.case_definitions == np.NAN):
+            print('There are some left over Nans in the case definition. Something went wrong.')
 
     def prepare_services(self):
         """ Interprets user given data and prepares it for each ValueStream (dispatch and pre-dispatch).

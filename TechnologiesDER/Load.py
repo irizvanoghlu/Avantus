c25d157c55a307532fca9d1ab60b13e5e2db5da9
phna001 <hnathwani@epri.com>
2020-03-19 13:30:04 -0700

    added energy evolution equations

--- TechnologiesDER/Load.py
+++ TechnologiesDER/Load.py
@@ -34,11 +34,12 @@ class Load(DER):
         DER.__init__(self, params['name'], 'Load', params)
         # input params  UNITS ARE COMMENTED TO THE RIGHT
         self.dt = params['dt']
-        self.rated_power = params['power_rated']
-        self.duration = params['duration']
-        self.controllable = params['controllable']
+        self.rated_power = params['power_rated']  # kW
+        self.duration = params['duration']  # hour
+        self.site_load = params['site_load']
+        self.energy_max = self.rated_power * self.duration
 
-        self.variable_names = {'power'}
+        self.variable_names = {'power', 'ene'}
 
     def add_vars(self, size):
         """ Adds optimization variables to dictionary
@@ -52,26 +53,40 @@ class Load(DER):
         Returns:
             Dictionary of optimization variables
         """
-        variables = {'power': cvx.Variable(shape=size, name='power')}
+        variables = {'power': cvx.Variable(shape=size, name='power'),
+                     'ene': cvx.Variable(shape=size, name='ene', nonneg=True)}
         return variables
 
-    # def objective_constraints(self, variables, mask, reservations, mpc_ene=None):
-    #     """ Builds the master constraint list for the subset of timeseries data being optimized.
-    #
-    #     Args:
-    #         variables (Dict): Dictionary of variables being optimized
-    #         mask (DataFrame): A boolean array that is true for indices corresponding to time_series data included
-    #             in the subs data set
-    #         reservations (Dict): Dictionary of energy and power reservations required by the services being
-    #             preformed with the current optimization subset
-    #         mpc_ene (float): value of energy at end of last opt step (for mpc opt)
-    #
-    #     Returns:
-    #         A list of constraints that corresponds the battery's physical constraints and its service constraints
-    #     """
-    #     constraint_list = []
-    #
-    #     return constraint_list
+    def objective_constraints(self, variables, mask, reservations, mpc_ene=None):
+        """ Builds the master constraint list for the subset of timeseries data being optimized.
+
+        Args:
+            variables (Dict): Dictionary of variables being optimized
+            mask (DataFrame): A boolean array that is true for indices corresponding to time_series data included
+                in the subs data set
+            reservations (Dict): Dictionary of energy and power reservations required by the services being
+                preformed with the current optimization subset
+            mpc_ene (float): value of energy at end of last opt step (for mpc opt)
+
+        Returns:
+            A list of constraints that corresponds the battery's physical constraints and its service constraints
+        """
+        power = variables['power']
+        energy = variables['ene']
+
+        constraint_list = []
+        # SOE EVALUATION EQUATIONS (one set for every day)
+        sub = mask.loc[mask]
+        for day in sub.index.dayofyear.unique():
+            day_mask = (day == sub.index.dayofyear)
+            # general
+            constraint_list += [cvx.Zero(energy[day_mask][:-1] + (power[day_mask][:-1] * self.dt) - energy[day_mask][1:])]
+            # start of first timestep of the day
+            constraint_list += [cvx.Zero(energy[day_mask][0] == self.energy_max)]
+            # end of the last timestep of the day
+            constraint_list += [cvx.Zero(energy[day_mask][-1] + (power[day_mask][-1] * self.dt) - self.energy_max)]
+
+        return constraint_list
 
     # def timeseries_report(self):
     #     """ Summaries the optimization results for this DER.
@@ -81,5 +96,8 @@ class Load(DER):
     #
     #     """
     #     results = pd.DataFrame(index=self.variables.index)
-    #     results[f"{'Controllable ' if self.controllable else ''}Load (kw)"] = self.variables['power']
+    #     if self.duration:
+    #         results["Controllable Load (kw)"] = self.site_load - self.variables['power']
+    #     else:
+    #         results["Load (kw)"] = self.site_load
     #     return results
